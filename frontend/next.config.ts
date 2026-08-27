fc2291338e405888b60ef2069a785fe9554cad3f
threfire <2359716419@qq.com>
2026-08-27 23:05:15 +0800

    同步视频上传代理容量限制

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "localhost", "10.*.*.*", "172.*.*.*", "192.168.*.*"],
+  experimental: {
+    proxyClientMaxBodySize: "62mb",
+  },
 };
 
 export default nextConfig;
